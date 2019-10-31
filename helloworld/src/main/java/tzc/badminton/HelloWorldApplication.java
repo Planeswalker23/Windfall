4746686ddde798df751d09f4450a8161b5613976
Planeswalker23 <805334683@qq.com>
2019-10-31 21:14:33 +0800

    add HelloWorld module

--- helloworld/src/main/java/tzc/badminton/HelloWorldApplication.java
+++ helloworld/src/main/java/tzc/badminton/HelloWorldApplication.java
@@ -4,14 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * springboot 启动类
+ * springboot启动器
  * @author Planeswalker23
- * @date Created in 2019-10-31
+ * @date Created in 2019-10-11
  */
 @SpringBootApplication
-public class WindfallApplication {
+public class HelloWorldApplication {
 
+    /**
+     * 启动springboot程序的主入口
+     * @param args
+     */
     public static void main(String[] args) {
-        SpringApplication.run(WindfallApplication.class, args);
+        SpringApplication.run(HelloWorldApplication.class, args);
     }
 }

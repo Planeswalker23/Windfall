1d6ebea179f8b9e38a38d081c29540271104ef77
Planeswalker23 <805334683@qq.com>
2019-10-31 21:02:51 +0800

    initialize project

---
+++ src/main/java/tzc/badminton/WindfallApplication.java
@@ -0,0 +1,17 @@
+package tzc.badminton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot 启动类
+ * @author Planeswalker23
+ * @date Created in 2019-10-31
+ */
+@SpringBootApplication
+public class WindfallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindfallApplication.class, args);
+    }
+}

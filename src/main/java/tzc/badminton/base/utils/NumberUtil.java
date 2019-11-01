74b3f00b3fda9e612e9adc7d6e5c8a9b0eabbd0b
Planeswalker23 <805334683@qq.com>
2019-11-02 00:55:36 +0800

    md5工具类修改

---
+++ src/main/java/tzc/badminton/base/utils/NumberUtil.java
@@ -0,0 +1,30 @@
+package tzc.badminton.base.utils;
+
+import org.springframework.util.DigestUtils;
+
+import java.util.UUID;
+
+/**
+ * 密码加密工具类
+ * @author Planeswalker23
+ * @date Created in 2019-11-01
+ */
+public class NumberUtil {
+
+    /**
+     * md5加密，所有密码参数需使用此方法加密
+     * @param password
+     * @return
+     */
+    public static String md5(String password) {
+        return DigestUtils.md5DigestAsHex(password.getBytes());
+    }
+
+    /**
+     * 生成UUID
+     * @return
+     */
+    public static String createUUId() {
+        return UUID.randomUUID().toString();
+    }
+}

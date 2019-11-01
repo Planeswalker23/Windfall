4e3dff7dd491cc39a19d603d422a721449c058d0
Planeswalker23 <805334683@qq.com>
2019-11-01 22:21:37 +0800

    基于tkMybatis新增持久层mapper及xml文件

---
+++ src/main/java/tzc/badminton/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package tzc.badminton.mapper;
+
+import tk.mybatis.mapper.common.Mapper;
+import tzc.badminton.module.User;
+
+/**
+ * user表 持久层
+ * @author Planeswalker23
+ * @date Created in 2019-11-01
+ */
+public interface UserMapper extends Mapper<User> {
+
+}

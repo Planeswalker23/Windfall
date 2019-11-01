98c4cda2019fb3edda607411b31ff4946130dfed
Planeswalker23 <fanyidong@biosan.cn>
2019-11-01 12:57:35 +0800

    增加controller层登录服务控制层

---
+++ src/main/java/tzc/badminton/controller/LoginController.java
@@ -0,0 +1,12 @@
+package tzc.badminton.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * todo 登录服务控制层
+ * @author Planeswalker23
+ * @date Created in 2019-11-01
+ */
+@RestController
+public class LoginController {
+}

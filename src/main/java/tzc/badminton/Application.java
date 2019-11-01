5e368059295ab74b636463920a4f9356563ee520
Planeswalker23 <fanyidong@biosan.cn>
2019-11-01 12:33:40 +0800

    修改Application类名

--- src/main/java/tzc/badminton/Application.java
+++ src/main/java/tzc/badminton/Application.java
@@ -6,16 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * springboot启动器
  * @author Planeswalker23
- * @date Created in 2019-10-11
+ * @date Created in 2019-10-31
  */
 @SpringBootApplication
-public class HelloWorldApplication {
+public class Application {
 
     /**
      * 启动springboot程序的主入口
      * @param args
      */
     public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }

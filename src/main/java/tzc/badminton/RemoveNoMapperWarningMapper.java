ef8f2a58cfa76b088335806a4cab36058ee99cf9
Planeswalker23 <805334683@qq.com>
2019-11-02 10:50:45 +0800

    新增消除警告日志的伪mapper类

---
+++ src/main/java/tzc/badminton/RemoveNoMapperWarningMapper.java
@@ -0,0 +1,14 @@
+package tzc.badminton;
+
+/**
+ * 使用的tk的开源项目进行mybatis集成，会抛出如下warn警告
+ *      No MyBatis mapper was found in '[tzc.badminton]' package. Please check your configuration.
+ *      为了消除此警告，建立这个mapper，可忽略
+ *
+ * 原因：在项目启动时，doScan()会扫描启动类同级目录下的mapper接口
+ * 但是合理的目录结果绝对不允许所有的mapper都在启动类目录下，所以在启动类目录下添加了一个伪mapper
+ * @author Planeswalker23
+ * @date Created in 2019-11-02
+ */
+public interface RemoveNoMapperWarningMapper {
+}

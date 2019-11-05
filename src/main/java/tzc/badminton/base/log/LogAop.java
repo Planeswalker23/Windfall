76986a2b937387951ebfc90b219f1b3761a54c99
Planeswalker23 <fanyidong@biosan.cn>
2019-11-05 10:22:54 +0800

    logAop使用org.slf4j.Logger

--- src/main/java/tzc/badminton/base/log/LogAop.java
+++ src/main/java/tzc/badminton/base/log/LogAop.java
@@ -29,54 +29,31 @@ public class LogAop {
      */
     @Around(value = "@annotation(log)")
     public Object outPutLogBeforeDoMethods(ProceedingJoinPoint pjp, Log log) throws Throwable {
-        // 输出流StringBuffer线程安全
-        StringBuffer outPutString = new StringBuffer();
-        outPutString.append("日志信息:\t").append(log.value()).append("\n");
+        logger.info("——————————Start: {}", log.value());
         // 接口类型
-        outPutString.append("Kind接口:\t").append(pjp.getKind()).append("\n");
+        logger.info("连接点类型: {}", pjp.getKind());
         // 类名
-        outPutString.append("Target:\t").append(pjp.getTarget().toString()).append("\n");
+        logger.info("目标对象: {}", pjp.getTarget());
         // 获取传入参数
         Object[] args = pjp.getArgs();
-        outPutString.append("Args:").append("\n");
-        // 存储传入参数
-        StringBuffer inputParams = new StringBuffer();
+        logger.info("Args参数: ");
         // 遍历参数数组
         for (int i = 0; i < args.length; i++) {
-            outPutString.append("\t==>input[").append(i).append("]:\t").append(args[i] == null ? "" : args[i].toString()).append("\n");
-            // 统计请求参数 防止查询后的结果过长 在业务执行前统计
-            try {
-                // 跳过Mock测试框架中Json解析MockRequest的步骤
-                String typeName = args[i].getClass().getName();
-                if (typeName.contains("Mock")) {
-                    inputParams.append(args[i].toString() + ",");
-                } else {
-                    inputParams.append(JSON.toJSONString(args[i]) + ",");
-                }
-            } catch (Exception e) {
-                try {
-                    inputParams.append(args[i].toString() + ",");
-                } catch (Exception e1) {
-                    continue;
-                }
-                continue;
-            }
+            logger.info("\t==>input[{}]:\t{}", i, JSON.toJSONString(args[i]));
         }
-        outPutString.append("Signature:\t").append(pjp.getSignature()).append("\n");
-        outPutString.append("SoruceLocation:\t").append(pjp.getSourceLocation()).append("\n");
-        outPutString.append("StaticPart:\t").append(pjp.getStaticPart()).append("\n");
+        logger.info("方法签名: {}", pjp.getSignature());
+        // 获取接口返回内容
         Object retVal = pjp.proceed();
         String retValStr = null;
         if (retVal instanceof String){
             retValStr = (String) retVal;
         } else if (retVal != null){
-            retValStr = JSON.toJSONString(new Object[]{retVal});
+            retValStr = JSON.toJSONString(retVal);
         }
         if (retValStr != null) {
-            outPutString.append("Result:\t").append(retValStr).append("\n");
+            logger.info("返回结果: {}", retValStr);
         }
-        outPutString.append("----------分割线----------").append("\n");
-        logger.info(outPutString.toString());
+        logger.info("——————————End: {}\n", log.value());
         return retVal;
     }
 }

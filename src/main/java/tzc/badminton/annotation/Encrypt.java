fc492c380c9a1d4f23fd67cf4004b4f07e6af78a
Planeswalker23 <805334683@qq.com>
2019-11-09 12:43:08 +0800

    新增修饰于属性的加密标识注解

--- src/main/java/tzc/badminton/annotation/Encrypt.java
+++ src/main/java/tzc/badminton/annotation/Encrypt.java
@@ -4,8 +4,9 @@ import java.lang.annotation.*;
 
 /**
  * 加密解密注解
- * 使用此注解修饰的数据库实体类属性，在insert或update或用作where条件到数据库中时会被加密
- *                             在select出来只有会被解密
+ * 使用此注解修饰的数据库实体类属性
+ *  在insert或update或用作where条件到数据库中时会被加密
+ *  在select出来只有会被解密
  * @author Planeswalker23
  * @date Created in 2019-11-08
  */
@@ -13,6 +14,4 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Encrypt {
-
-    // todo 加密规则、是否使用key等参数
 }

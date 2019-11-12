64c6f1857343edb3e3683235acc71db8e5272623
Planeswalker23 <fanyidong@biosan.cn>
2019-11-12 11:01:46 +0800

    修改返回类型为经过策略类修改过的对象，是为了使用Lists.transform()

--- src/main/java/tzc/badminton/config/crypt/CryptExecutor.java
+++ src/main/java/tzc/badminton/config/crypt/CryptExecutor.java
@@ -29,8 +29,9 @@ public class CryptExecutor {
      * 根据指定的策略类对指定的对象target进行解码、加密操作
      * @param target
      * @param clazz 必须实现了 CryptStrategy 接口
+     * @return Object 此对象可以不接收，因为对象已经被修改
      */
-    public void cryptForSingleObject(Object target, Class<? extends CryptStrategy> clazz) {
+    public Object cryptForSingleObject(Object target, Class<? extends CryptStrategy> clazz) {
         // 根据cryptType从工厂中生成具体的策略类
         cryptStrategy = CryptFactory.build(clazz);
         // 根据类及注解类获取类中的使用注解修饰的属性列表
@@ -43,5 +44,6 @@ public class CryptExecutor {
             // 具体的执行策略
             cryptStrategy.run(target, value, field.getName());
         });
+        return target;
     }
 }

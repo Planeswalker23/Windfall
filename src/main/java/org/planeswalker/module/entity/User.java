66df7226978db2648b8779cea53020e99decab6f
Planeswalker23 <fanyidong@biosan.cn>
2019-12-05 13:16:41 +0800

    开启Mybatis下划线命名转驼峰命名

--- src/main/java/org/planeswalker/module/entity/User.java
+++ src/main/java/org/planeswalker/module/entity/User.java
@@ -1,9 +1,7 @@
 package org.planeswalker.module.entity;
 
 import lombok.Data;
-import org.planeswalker.annotation.Encrypt;
 
-import javax.persistence.Column;
 import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Date;
@@ -20,19 +18,16 @@ public class User implements Serializable {
      * user表 主键
      */
     @Id
-    @Column(name = "user_id")
     private String userId;
 
     /**
      * 用户名
      */
-    @Column(name = "user_name")
     private String userName;
 
     /**
      * 密码，使用md5加密，代码中加密
      */
-    @Encrypt
     private String password;
 
     /**
@@ -43,12 +38,10 @@ public class User implements Serializable {
     /**
      * 创建记录的时间
      */
-    @Column(name = "create_time")
     private Date createTime;
 
     /**
      * 操作时间，写入数据库时自动更新
      */
-    @Column(name = "update_time")
     private Date updateTime;
 }

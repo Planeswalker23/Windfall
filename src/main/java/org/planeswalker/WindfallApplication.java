bd4034d8ad5d986ef9981306a9315389e7b0340b
Planeswalker23 <fanyidong@biosan.cn>
2019-12-06 09:22:22 +0800

    mybatis-plus乐观锁需要注入乐观锁拦截器

--- src/main/java/org/planeswalker/WindfallApplication.java
+++ src/main/java/org/planeswalker/WindfallApplication.java
@@ -1,9 +1,12 @@
 package org.planeswalker;
 
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * springboot启动器
@@ -12,7 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date Created in 2019-10-31
  */
 @SpringBootApplication
-@tk.mybatis.spring.annotation.MapperScan(basePackages = "org.planeswalker.mapper")
+@MapperScan("org.planeswalker.mapper")
 public class WindfallApplication {
 
     private static Logger logger = LoggerFactory.getLogger(WindfallApplication.class);
@@ -24,5 +27,14 @@ public class WindfallApplication {
     public static void main(String[] args) {
         SpringApplication.run(WindfallApplication.class, args);
         logger.info("==================启动成功================");
+    }
+
+    /**
+     * 乐观锁拦截器
+     * @return {@link com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor}
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
     }
 }

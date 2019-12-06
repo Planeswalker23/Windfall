aae305ffad49c9a025b46f59578e6225e39eb063
Planeswalker23 <fanyidong@biosan.cn>
2019-12-06 11:12:55 +0800

    fix: 过滤器的异常不进入统一异常处理机制，需要使用输出流返回

--- src/main/java/org/planeswalker/config/VerifyLoginFilter.java
+++ src/main/java/org/planeswalker/config/VerifyLoginFilter.java
@@ -1,12 +1,16 @@
 package org.planeswalker.config;
 
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
 import org.planeswalker.base.Constant;
 import org.planeswalker.base.LoginErrors;
+import org.planeswalker.base.Response;
 import org.planeswalker.exception.LoginException;
+import org.planeswalker.exception.NotLoginException;
 import org.planeswalker.pojo.entity.User;
 import org.planeswalker.utils.SessionUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.*;
@@ -14,6 +18,7 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -45,7 +50,15 @@ public class VerifyLoginFilter implements Filter {
         logger.info("==================验证登录过滤器================");
         Map<String, String[]> parameters = request.getParameterMap();
         // 登录验证，此方法中已包含未登录的验证
-        User userBean = SessionUtil.getUserBean();
+        User userBean;
+        // fix: 过滤器的异常不进入统一异常处理机制，需要使用输出流返回
+        try {
+            userBean = SessionUtil.getUserBean();
+        } catch (NotLoginException e) {
+            logger.warn(e.getMessage(), e);
+            this.returnJson(httpServletResponse, e);
+            return;
+        }
         String[] userIdArray = parameters.get(Constant.USER_ID);
         // 获取参数中的userId参数数组
         String userId = null;
@@ -73,5 +86,34 @@ public class VerifyLoginFilter implements Filter {
             return true;
         }
         return false;
+    }
+
+    /**
+     * 过滤器若出现未登录异常，返回输出流
+     * fix: 过滤器的异常不进入统一异常处理机制
+     * @param response
+     * @param exception
+     */
+    private void returnJson(HttpServletResponse response, Exception exception){
+        OutputStream writer = null;
+        response.setCharacterEncoding(OutputFormat.Defaults.Encoding);
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        try {
+            writer = response.getOutputStream();
+            writer.write(Response.failed(exception.getMessage()).getBytes());
+            // 清空缓存并输出流
+            writer.flush();
+        } catch (IOException e){
+            logger.error("过滤器输出流异常: {}", e.getMessage(), e);
+        } finally {
+            // 关闭输出流
+            if(writer != null){
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    logger.error("输出流关闭异常: {}", e.getMessage(), e);
+                }
+            }
+        }
     }
 }

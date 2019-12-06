d8044991be8511a42563d22d981c677843c496cb
Planeswalker23 <fanyidong@biosan.cn>
2019-12-06 13:42:15 +0800

    提取公用的查询方法，在服务方法中只进行业务逻辑

--- src/main/java/org/planeswalker/service/LoginService.java
+++ src/main/java/org/planeswalker/service/LoginService.java
@@ -5,7 +5,6 @@ import org.planeswalker.base.Constant;
 import org.planeswalker.base.Errors;
 import org.planeswalker.base.LoginErrors;
 import org.planeswalker.exception.LoginException;
-import org.planeswalker.exception.WindfallException;
 import org.planeswalker.mapper.UserMapper;
 import org.planeswalker.pojo.dto.LoginDto;
 import org.planeswalker.pojo.entity.User;
@@ -47,30 +46,22 @@ public class LoginService {
      *         修改成功返回null
      */
     public String applyUser(User newUser) {
-        String email = newUser.getEmail();
-        // 邮箱是否为空
-        boolean isEmailEmpty = StringUtils.isEmpty(email);
         // 验证邮箱格式，邮箱用于找回密码
-        if (!isEmailEmpty && !CheckUtil.checkEmail(email)) {
-            throw new WindfallException(LoginErrors.WRONG_MAIL);
-        }
+        CheckUtil.checkEmail(newUser.getEmail());
         // 根据「邮箱」查询所有匹配的用户
-        List<User> resultByEmail = userMapper.selectList(Wrappers.<User>lambdaQuery()
-                .eq(User::getEmail, newUser.getEmail()));
-        if (!CollectionUtils.isEmpty(resultByEmail) && resultByEmail.size() > 0) {
-            // 若存在邮箱不相等，且userId不相等的情况，说明想要修改或注册的邮箱已经被使用
-            // 注册时根据已存在邮箱查询到的结果userId不等于传参userId:null
-            resultByEmail.forEach(resultEntity ->{
-                if (email.equals(resultEntity.getEmail()) && !resultEntity.getUserId().equals(newUser.getUserId())) {
-                    // 若传入userId为空是注册报错，否则是修改个人信息的报错
-                    if (StringUtils.isEmpty(newUser.getUserId())) {
-                        throw new LoginException(LoginErrors.MAIL_EXIST);
-                    } else {
-                        throw new LoginException(LoginErrors.USER_NOT_EXIST);
-                    }
+        List<User> resultByEmail = this.getUserByEmail(newUser.getEmail());
+        // 若存在邮箱不相等，且userId不相等的情况，说明想要修改或注册的邮箱已经被使用
+        // 注册时根据已存在邮箱查询到的结果userId不等于传参userId:null
+        resultByEmail.forEach(resultEntity ->{
+            if (newUser.getEmail().equals(resultEntity.getEmail()) && !resultEntity.getUserId().equals(newUser.getUserId())) {
+                // 若传入userId为空是注册报错，否则是修改个人信息的报错
+                if (StringUtils.isEmpty(newUser.getUserId())) {
+                    throw new LoginException(LoginErrors.MAIL_EXIST);
+                } else {
+                    throw new LoginException(LoginErrors.USER_NOT_EXIST);
                 }
-            });
-        }
+            }
+        });
         if (StringUtils.isEmpty(newUser.getUserId())) {
             newUser.setPassword(newUser.getPassword());
             // 添加其他字段
@@ -108,41 +99,63 @@ public class LoginService {
      */
     public User login(LoginDto loginDto) {
         // 根据「邮箱」查询
-        List<User> usersBySameEmail = userMapper.selectList(Wrappers.<User>lambdaQuery()
-                .eq(User::getEmail, loginDto.getEmail()));
-        // 该邮箱尚未注册
-        if (CollectionUtils.isEmpty(usersBySameEmail)) {
-            throw new LoginException(LoginErrors.MAIL_NOT_REGISTER);
-        }
+        List<User> sameEmailUsers = this.getUserByEmail(loginDto.getEmail());
         // 验证是否根据邮箱只查询出一条记录
-        CollectionUtil.isOneDate(usersBySameEmail);
-        User selectByEmailUser = usersBySameEmail.get(Constant.ZERO);
+        CollectionUtil.isOneDate(sameEmailUsers);
+        User sameEmailUser = sameEmailUsers.get(Constant.ZERO);
         // 验证密码
-        if (!selectByEmailUser.getPassword().equals(loginDto.getPassword())) {
+        if (!sameEmailUser.getPassword().equals(loginDto.getPassword())) {
             throw new LoginException(LoginErrors.WRONG_PASSWORD);
         }
-        logger.info("登录成功，用户信息: {}", JacksonUtil.toJson(selectByEmailUser));
+        logger.info("登录成功，用户信息: {}", JacksonUtil.toJson(sameEmailUser));
         // 将已登录用户信息放入session
         HttpSession session = SessionUtil.getSession();
-        session.setAttribute(Constant.USER_BEAN, selectByEmailUser);
+        session.setAttribute(Constant.USER_BEAN, sameEmailUser);
         logger.info("登录成功, {} 的sessionId ==> {} 登录信息 ==> {}",
-                Constant.USER_BEAN, session.getId(), JacksonUtil.toJson(selectByEmailUser));
+                Constant.USER_BEAN, session.getId(), JacksonUtil.toJson(sameEmailUser));
         // 登录成功后返回用户信息
-        return selectByEmailUser;
+        return sameEmailUser;
     }
 
     /**
      * 根据userId获取用户个人信息
      * @param userId
-     * @return
+     * @return {@link User}
      */
     public User getUserInfo(String userId) {
         // 根据「userId」获取用户信息
-        User selectByUserIdEntity = userMapper.selectById(userId);
-        if (selectByUserIdEntity == null) {
-            throw new LoginException(LoginErrors.USER_NOT_EXIST);
-        }
+        User selectByUserIdEntity = this.getUserByUserId(userId);
         logger.info("用户个人信息: {} ", JacksonUtil.toJson(selectByUserIdEntity));
         return selectByUserIdEntity;
+    }
+
+    /**
+     * 根据邮箱查询所有匹配的user
+     * @param email not null or empty
+     * @return List<User> {@link User}
+     * @throws {@link LoginException}
+     */
+    private List<User> getUserByEmail(String email) throws LoginException {
+        List<User> sameEmailUsers = userMapper.selectList(Wrappers.<User>lambdaQuery()
+                .eq(User::getEmail, email));
+        // 该邮箱尚未注册
+        if (CollectionUtils.isEmpty(sameEmailUsers)) {
+            throw new LoginException(LoginErrors.MAIL_NOT_REGISTER);
+        }
+        return sameEmailUsers;
+    }
+
+    /**
+     * 根据userId查询唯一匹配的user
+     * @param userId
+     * @return {@link User}
+     * @throws {@link LoginException}
+     */
+    private User getUserByUserId(String userId) throws LoginException {
+        User sameUserIdUser = userMapper.selectById(userId);
+        if (sameUserIdUser == null) {
+            throw new LoginException(LoginErrors.USER_NOT_EXIST);
+        }
+        return sameUserIdUser;
     }
 }

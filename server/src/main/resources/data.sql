3934017e185f959373b6d7769ce8aa77e10738e5
Planeswalker23 <805334683@qq.com>
2020-02-04 14:11:32 +0800

    更新初始化 SQL 脚本

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into user (user_id, user_name, password, email, create_time, update_time, version) values ('root', 'root', 'root', 'root@qq.com', now(), now(), 0);
 
 insert into user_info (user_id, favourite, signature, create_time, update_time, version) values ('root', '1,2,3,4', 'root的个性签名', now(), now(), 0);
+
+insert into comment (comment_id, comment_pid, user_id, content, like_num, create_time, update_time, version) values ('rootComment', '0', 'root', 'root评测内容', '0', now(), now(), 0);
+insert into comment (comment_id, comment_pid, user_id, content, like_num, create_time, update_time, version) values ('rootComment2', 'rootComment', 'root', 'root评测内容的留言', '0', now(), now(), 0);

fc9a7b0273a0d8155c58ffb1d078172583f4a997
Planeswalker23 <805334683@qq.com>
2020-02-06 09:20:20 +0800

    优化首页布局，修改登录注册布局逻辑

--- server/src/main/resources/static/js/user.js
+++ server/src/main/resources/static/js/user.js
@@ -54,22 +54,3 @@ function edit() {
         }
     });
 }
-
-// 快速注册函数
-function quickRegister() {
-    $.ajax({
-        type: "POST",
-        url: "/user/register",
-        data: $('#quickRegister').serialize(),// 获取form表单中的数据
-        dataType: "json",// 预期服务器返回的数据类型
-        success:function (data) {
-            if (data.success) {
-                // 注册成功转到登录页面
-                window.location.href="/"; //在原有窗口打开
-            } else {
-                // 注册失败提示
-                alert(data.message);
-            }
-        }
-    });
-}

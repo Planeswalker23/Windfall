16db2aca02e9d010bd517f69ba006828ea709a9a
ListJ <619471333@qq.com>
2020-02-07 12:33:18 +0800

    发布问题

---
+++ server/src/main/java/org/planeswalker/controller/PublishController.java
@@ -0,0 +1,46 @@
+package org.planeswalker.controller;
+
+import org.planeswalker.pojo.entity.Comment;
+import org.planeswalker.pojo.entity.User;
+import org.planeswalker.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Controller
+public class PublishController {
+    @Autowired
+    private CommentService commentService;
+
+    @GetMapping("/publish")
+    public String publish(){
+        return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(@RequestParam(value = "title",required = false) String title,
+                          @RequestParam(value = "content",required = false) String content,
+                          @RequestParam(value = "id",required = false) String id,
+                          HttpServletRequest request,
+                          Model model){
+        User user=(User)request.getSession().getAttribute("user");
+        if(user==null){
+            model.addAttribute("error","用户未登录");
+            return "publish";
+        }
+        Comment comment=new Comment();
+        comment.setCommentId(id);
+        comment.setUserId(user.getUserId());
+        comment.setTitle(title);
+        comment.setContent(content);
+        commentService.addComment(comment);
+        return "publish";
+    }
+
+}
